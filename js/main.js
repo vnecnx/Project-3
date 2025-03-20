e6b3dbfa786572b42c984f9d5e5049d4e74358dc
vnecnx <ernestkfowler@gmail.com>
2025-03-20 11:23:55 -0500

    Base work

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
+// Lesson.html stuff
+// const squaretl = gsap.timeline();
+// const squarebutton = document.querySelector('.square');
+// const squarebuttonactive = document.querySelector('.square.active');
+
+// squarebutton.addEventListener('click', () =>{
+//     squaretl.to(".square", {duration: 1, ease:"bounce.out", x: 500, rotation: 360}, "start")
+//             // .to(".square", {duration: 1, ease:"power4.out", backgroundColor: "blue", onComplete: function() {
+//             //     this.targets().forEach(elem => elem.classList.add("active"));
+//             //   }}}, "start")
+//             .to(".square", {duration: 1, ease:"power4.out", backgroundColor: "blue", onComplete: function() {
+//                 this.targets().forEach(elem => elem.classList.add("active"));
+//               }})
+// })
+
+// squarebuttonactive.addEventListener('click', () =>{
+//     console.log*('clicked');
+//     squaretl.reverse()
+// })
+
+
 // JS scripts placed here
+
+const button = document.querySelector('.snooze-container');
+const body = document.querySelector('body');
+
+
+
+// button.addEventListener('click', () => {
+//   const expanded = button.getAttribute('aria-expanded') === 'true' || false;
+//   button.setAttribute('aria-expanded',!expanded);
+//   body.classList.toggle('expanded');
+// });
