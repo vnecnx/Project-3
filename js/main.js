c2ca7b21786d6617abfc33ee2066440706955b33
vnecnx <ernestkfowler@gmail.com>
2025-03-27 10:31:12 -0500

    Changed button

--- js/main.js
+++ js/main.js
@@ -30,9 +30,13 @@ button.addEventListener('click', () =>{
     bodytl.to(".wine-liquid", {duration: 1, ease: "power4.out", backgroundColor:"#aac3f7"}, "start")
     bodytl.to("body", {duration: 1, ease: "power4.out", backgroundColor: "rgb(240, 233, 213)"}, "start")
     bodytl.to(".clock-display", {duration: 1, ease: "power4.out", backgroundColor: "#2f2f2f", boxShadow: "none"}, "start") 
-    bodytl.to(".clock-display-text", {duration: 1, ease: "power4.out", color: "#36db78"}, "start") 
-    bodytl.to(".snooze-container p", {duration: 1, ease: "power4.out", color: "black"}, "start") 
+    bodytl.to(".clock-display-text", {duration: 1, ease: "power4.out", color: "#36db78"}, "start")
+    bodytl.to(".snooze-container", {duration: 1, ease: "power4.out", boxShadow: "none"}, "start") 
+    bodytl.to(".snooze-container p", {duration: 1, ease: "power4.out", color: "black"}, "start")
+    bodytl.to(".snooze-container", {duration: 1, ease: "power4.out", backgroundColor: "rgb(243, 244, 255)", boxShadow: "8px 8px 0 0 #afafff"}, "start")
 });
+
+
 
 button.addEventListener('click', () =>{
     buttontext.innerHTML = "SNOOZE";
@@ -49,8 +53,8 @@ const chainbutton = document.querySelector('.lamp-chain');
 
 chainbutton.addEventListener('click', () =>{
     console.log('clicked');
-    // lamptl.to(".lamp-bulb", {duration: 1, ease: "power4.out", backgroundColor: "rgb(255, 236, 62)"}, "lighton")
     lamptl.to(".lamp-chain", {duration: 0.5, ease: "power4.out", y: 20})
+    lamptl.to(".lamp-bulb", {duration: 1, ease: "bounce.in", backgroundColor: "rgb(255, 236, 62)"}, "lighton")
     lamptl.to(".lamp-chain", {duration: 0.5, ease: "power4.out", y: 0}, "lighton")
     lamptl.to(".lamp-light", {duration: 1, ease: "bounce.in", opacity: ".5"}, "lighton")
     // lamptl.to(".lamp-light", {duration: 0.2, ease: "power4.out", opacity: "0"})
