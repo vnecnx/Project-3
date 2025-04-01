aa10f269cca01f2952494232e1c5a601d0ae4ec3
vnecnx <ernestkfowler@gmail.com>
2025-04-01 12:17:09 -0500

    Made button reversable, but is not loopable.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,32 @@
 // SNOOZE BUTTON
 
-const bodytl = gsap.timeline();
+
 const button = document.querySelector('.snooze-container');
 const buttontext = document.querySelector('.snooze-container p');
+const buttonactive = document.querySelector('.buttonactive');
+const bodytl = gsap.timeline(
+    button.addEventListener('click', () =>{
+        bodytl.to(".wine-liquid", {duration: 1, ease: "power4.out", backgroundColor:"#aac3f7", onStart: () => {
+            console.log("Animation started!");
+            button.classList.remove('buttonactive');}}, "start")
+            .to("body", {duration: 1, ease: "power4.out", backgroundColor: "rgb(240, 233, 213)"}, "start")
+            .to(".clock-display", {duration: 1, ease: "power4.out", backgroundColor: "#2f2f2f", boxShadow: "none"}, "start") 
+            .to(".clock-display-text", {duration: 1, ease: "power4.out", color: "#36db78"}, "start")
+            .to(".snooze-container", {duration: 1, ease: "power4.out", boxShadow: "none"}, "start")
+            .to(".snooze-container p", {duration: 1, ease: "power4.out", color: "black"}, "start")
+            .to(".snooze-container", {duration: 1, ease: "power4.out", backgroundColor: "rgb(243, 244, 255)", boxShadow: "8px 8px 0 0 #afafff", 
+                onComplete: () => {
+                console.log("Animation completed");
+                buttonactive.addEventListener('click', () => {
+                    bodytl.reverse();
+                    button.classList.add('buttonactive');
+                })
+                }}, 
+                "start")
+    })
+);
 
-button.addEventListener('click', () =>{
-    bodytl.to(".wine-liquid", {duration: 1, ease: "power4.out", backgroundColor:"#aac3f7", onStart: () => {
-        console.log("Animation started!");
-        buttonelement.classList.add('buttonactive');}}, "start")
-        .to("body", {duration: 1, ease: "power4.out", backgroundColor: "rgb(240, 233, 213)"}, "start")
-        .to(".clock-display", {duration: 1, ease: "power4.out", backgroundColor: "#2f2f2f", boxShadow: "none"}, "start") 
-        .to(".clock-display-text", {duration: 1, ease: "power4.out", color: "#36db78"}, "start")
-        .to(".snooze-container", {duration: 1, ease: "power4.out", boxShadow: "none"}, "start") 
-        .to(".snooze-container p", {duration: 1, ease: "power4.out", color: "black"}, "start")
-        .to(".snooze-container", {duration: 1, ease: "power4.out", backgroundColor: "rgb(243, 244, 255)", boxShadow: "8px 8px 0 0 #afafff"}, "start")
-});
+
 
 button.addEventListener('click', () =>{
     buttontext.innerHTML = "SNOOZE";
